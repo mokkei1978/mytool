c51d75edaba69e5e80728a2a714485526c49afe6
ksakamot <ksakamot@mri-jma.go.jp>
2015-02-13 15:32:54 +0900

    DATA/topo10m をバックアップ対象に加える

--- backup/backup_data.sh
+++ backup/backup_data.sh
@@ -6,5 +6,6 @@ rsync -a ocsv001:/worka/ksakamot/modis ~/DATA/
 rsync -a ocsv001:/worka/ksakamot/CORE2jpn/data ~/DATA/CORE2jpn/
 rsync -a ocsv001:/worka/ksakamot/rectangle/data ~/DATA/rectangle/
 rsync -a ocsv001:/worka/ksakamot/seto/data ~/DATA/seto/
+rsync -a ocsv001:/worka/ksakamot/topo10m ~/DATA/
 
 
