ac04f3cf28d5dc767539de26a0ae79f1add8101a
Kei Sakamoto <ksakamot@mri-jma.go.jp>
2022-08-29 13:15:45 +0900

    Add png to Wiki.

--- wiki/export_html.sh
+++ wiki/export_html.sh
@@ -7,9 +7,11 @@ dir=mri.com-user
 #dir="."
 current_dir=`pwd`
 
+org_dir="/home/ocpublic/public_html/wiki/data"
+
 
 #- Get file names.
-cd /home/ocpublic/public_html/wiki/data/pages
+cd ${org_dir}/pages
 files=`find ${dir} -name "*.txt"`
 
 
@@ -17,7 +19,7 @@ files=`find ${dir} -name "*.txt"`
 #- Download html pages.
 cd ~/public_html
 rm -rf html
-mkdir -p html
+mkdir -p html/media/mri.com-user
 
 for file in ${files}; do
 
@@ -47,9 +49,14 @@ sed -i "/<link rel=/d" html/*.html
 sed -i -e '13i <link rel="stylesheet" type="text/css" href="standard.css">' html/*.html
 sed -i -e '20i <div class="level1"><a href="index.html">MRI.COM</a></div>' html/*.html
 
+#- Change image link
+sed -i -e "s@/~ocpublic/wiki/lib/exe/detail.php?[^:]*media@media@g" \
+    -e "s@/~ocpublic/wiki/lib/exe/fetch.php?[^:]*media@media@g" html/*.html
+sed -i -e "/media/s@media=@media/@g" -e "/media/s@:@/@g" html/*.html
+
 #- Remove ":"
 cd html
-files=`ls .`
+files=`ls *.html`
 for file in ${files}; do
   sed -i -e "/wikilink/s/\:/_/g" -e "s/http_/http\:/g" -e "s/xml_lang/xml:lang/g" ${file}
   file2=`echo ${file} | sed -e "s%:%_%g"`
@@ -60,8 +67,11 @@ done
 ln -s ${dir}_start.html index.html
 cp ${current_dir}/standard.css .
 cp ${current_dir}/link_icon.gif .
-cd ..
-#zip html.zip html/*
+
+
+#### Copy image files.
+rsync -a ${org_dir}/media/mri.com-user/ media/mri.com-user/
+
 
 echo "Make http://oceansv.i.mri-jma.go.jp/~ksakamot/html/"
 
